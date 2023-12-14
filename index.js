04a172d2f15b1e5c408130005c8b47d8ad1d0d07
dimplestinto <dimples.tinto@gmail.com>
2023-12-14 12:07:01 +0800

    init: mongodb connection setup

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import limiter from "./libs/rate-limiter.js";
 import { authRouter } from "./routes/auth.js";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -24,9 +26,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(limiter);
 app.set("trust proxy", 1);
+app.use(morgan("tiny"));
 app.disable("x-powered-by");
 
 app.use("/api/v1/auth", authRouter);
+
+try {
+	(async () => {
+		await mongoose.connect(process.env.MONGODB_URI).then(() => {
+			console.log(`🚀 Connected to MongoDB  `);
+		});
+	})();
+} catch (err) {
+	console.log(err);
+	throw new Error("Error connecting to db", err);
+}
 
 app.listen(PORT, () => {
 	console.log(`⚡️ Server is running on port ${PORT}`);
